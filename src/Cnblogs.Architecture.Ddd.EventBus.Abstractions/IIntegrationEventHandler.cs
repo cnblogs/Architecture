7d442dcf291e7459db2e603f8d00f56b5c246f2e
dudu <dudu@cnblogs.com>
2023-02-19 14:31:04 +0800

    refactor: introduce non-generic IIntegrationEventHandler

--- src/Cnblogs.Architecture.Ddd.EventBus.Abstractions/IIntegrationEventHandler.cs
+++ src/Cnblogs.Architecture.Ddd.EventBus.Abstractions/IIntegrationEventHandler.cs
@@ -1,4 +1,4 @@
-﻿using MediatR;
+using MediatR;
 
 namespace Cnblogs.Architecture.Ddd.EventBus.Abstractions;
 
@@ -6,7 +6,16 @@ namespace Cnblogs.Architecture.Ddd.EventBus.Abstractions;
 /// 集成事件处理器。
 /// </summary>
 /// <typeparam name="TEvent">集成事件。</typeparam>
-public interface IIntegrationEventHandler<TEvent> : INotificationHandler<TEvent>
+#pragma warning disable SA1402 // File may only contain a single type
+public interface IIntegrationEventHandler<TEvent> : INotificationHandler<TEvent>, IIntegrationEventHandler
+#pragma warning restore SA1402 // File may only contain a single type
     where TEvent : IntegrationEvent
+{
+}
+
+/// <summary>
+/// The non-generic interface as a generic type constraint
+/// </summary>
+public interface IIntegrationEventHandler
 {
 }
