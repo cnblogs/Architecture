475aeca7a79183b871236556ef122b851c9f18a9
沈星繁 <ikesnowy@outlook.com>
2023-12-13 12:50:45 +0800

    test: add title

--- test/Cnblogs.Architecture.UnitTests/Infrastructure/FakeObjects/FakeDbContext.cs
+++ test/Cnblogs.Architecture.UnitTests/Infrastructure/FakeObjects/FakeDbContext.cs
@@ -14,10 +14,12 @@ public class FakeDbContext : DbContext
     {
         modelBuilder.Entity<FakeBlog>().HasKey(x => x.Id);
         modelBuilder.Entity<FakeBlog>().Property(x => x.Id).ValueGeneratedOnAdd();
+        modelBuilder.Entity<FakeBlog>().Property(x => x.Title).HasMaxLength(30);
         modelBuilder.Entity<FakeBlog>().HasMany(x => x.Posts).WithOne(x => x.Blog).HasForeignKey(x => x.BlogId);
 
         modelBuilder.Entity<FakePost>().HasKey(x => x.Id);
         modelBuilder.Entity<FakePost>().Property(x => x.Id).ValueGeneratedOnAdd();
+        modelBuilder.Entity<FakePost>().Property(x => x.Title).HasMaxLength(100);
         modelBuilder.Entity<FakePost>().HasMany(x => x.Tags).WithOne().HasForeignKey(x => x.PostId);
 
         modelBuilder.Entity<FakeTag>().HasKey(x => x.Id);
