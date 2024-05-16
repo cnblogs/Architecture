095f5327757aa4d7e090da428f609cc43b85629c
沈星繁 <ikesnowy@outlook.com>
2024-05-16 16:09:51 +0800

    feat: support async query&command creation on minimal api mapping

--- test/Cnblogs.Architecture.IntegrationTestProject/Program.cs
+++ test/Cnblogs.Architecture.IntegrationTestProject/Program.cs
@@ -7,6 +7,7 @@ using Cnblogs.Architecture.IntegrationTestProject.Application.Commands;
 using Cnblogs.Architecture.IntegrationTestProject.Application.Queries;
 using Cnblogs.Architecture.IntegrationTestProject.Payloads;
 using Cnblogs.Architecture.TestIntegrationEvents;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -36,10 +37,18 @@ app.Subscribe<TestIntegrationEvent>();
 
 var apis = app.NewVersionedApi();
 var v1 = apis.MapGroup("/api/v{version:apiVersion}").HasApiVersion(1);
-v1.MapQuery<GetStringQuery>("apps/{appId}/strings/{stringId:int}/value", MapNullableRouteParameter.Enable, enableHead: true);
-v1.MapQuery<GetStringQuery>("strings/{id:int}");
+v1.MapQuery<GetStringQuery>(
+    "apps/{appId}/strings/{stringId:int}/value",
+    MapNullableRouteParameter.Enable,
+    enableHead: true);
+v1.MapQuery(
+    "strings/{stringId:int}",
+    async (int stringId, [FromQuery] bool found)
+        => await Task.FromResult(new GetStringQuery(StringId: stringId, Found: found)));
 v1.MapQuery<ListStringsQuery>("strings");
-v1.MapCommand("strings", (CreatePayload payload) => new CreateCommand(payload.NeedError, payload.Data));
+v1.MapCommand(
+    "strings",
+    (CreatePayload payload) => Task.FromResult(new CreateCommand(payload.NeedError, payload.Data)));
 v1.MapCommand(
     "strings/{id:int}",
     (int id, UpdatePayload payload) => new UpdateCommand(id, payload.NeedValidationError, payload.NeedExecutionError));
