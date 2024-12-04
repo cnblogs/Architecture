bce2b9712c54fa4c3077b2eb7fe7e4df3f60cbb0
沈星繁 <ikesnowy@outlook.com>
2024-12-04 13:45:00 +0800

    chore: split testing packages by target frameworks

--- src/Cnblogs.Architecture.Ddd.Cqrs.AspNetCore/CqrsResult.cs
+++ src/Cnblogs.Architecture.Ddd.Cqrs.AspNetCore/CqrsResult.cs
@@ -6,7 +6,8 @@ namespace Cnblogs.Architecture.Ddd.Cqrs.AspNetCore;
 /// <summary>
 ///     Send object as json and append X-Cqrs-Version header
 /// </summary>
-/// <param name="commandResponse"></param>
+/// <param name="commandResponse">Response body.</param>
+/// <param name="options"><see cref="JsonSerializerOptions"/> to use.</param>
 public class CqrsResult(object commandResponse, JsonSerializerOptions? options = null) : IResult
 {
     /// <inheritdoc />
