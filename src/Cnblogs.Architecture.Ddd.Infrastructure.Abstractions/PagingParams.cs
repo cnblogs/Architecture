ae4234495a3e7fc4cb48da518b20ceb4095e14ab
dudu <dudu@cnblogs.com>
2025-10-08 12:51:22 +0800

    feat: bind PagingParams from route values (#335)
    * feat: bind PagingParams from route values
    
    * refactor: remove unnecessary pageIndex and pageSize check

--- src/Cnblogs.Architecture.Ddd.Infrastructure.Abstractions/PagingParams.cs
+++ src/Cnblogs.Architecture.Ddd.Infrastructure.Abstractions/PagingParams.cs
@@ -1,6 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 
 namespace Cnblogs.Architecture.Ddd.Infrastructure.Abstractions;
 
@@ -26,9 +27,23 @@ public record PagingParams([Range(1, int.MaxValue)] int PageIndex, [Range(0, int
     {
         const string pageIndexKey = "pageIndex";
         const string pageSizeKey = "pageSize";
-        var hasPageIndex = int.TryParse(context.Request.Query[pageIndexKey], out var pageIndex);
-        var hasPageSize = int.TryParse(context.Request.Query[pageSizeKey], out var pageSize);
-        if (hasPageIndex && hasPageSize && pageIndex > 0 && pageSize >= 0)
+
+        var routeValues = context.GetRouteData().Values;
+        object? pageSizeValue = null;
+        var inRouteValues =
+            routeValues.TryGetValue(pageIndexKey, out var pageIndexValue) &&
+            routeValues.TryGetValue(pageSizeKey, out pageSizeValue);
+
+        if (inRouteValues == false)
+        {
+            pageIndexValue = context.Request.Query[pageIndexKey];
+            pageSizeValue = context.Request.Query[pageSizeKey];
+        }
+
+        if (pageIndexValue != null &&
+            pageSizeValue != null &&
+            int.TryParse(pageIndexValue.ToString(), out var pageIndex) &&
+            int.TryParse(pageSizeValue.ToString(), out var pageSize))
         {
             return ValueTask.FromResult<PagingParams?>(new PagingParams(pageIndex, pageSize));
         }
