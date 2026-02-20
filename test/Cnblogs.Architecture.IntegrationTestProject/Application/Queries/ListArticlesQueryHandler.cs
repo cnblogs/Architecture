905df818b5c7579e0249e09d6f2e2d560d12dfa4
沈星繁 <ikesnowy@outlook.com>
2026-02-20 13:59:05 +0800

    test: using test data for queries

--- test/Cnblogs.Architecture.IntegrationTestProject/Application/Queries/ListArticlesQueryHandler.cs
+++ test/Cnblogs.Architecture.IntegrationTestProject/Application/Queries/ListArticlesQueryHandler.cs
@@ -1,6 +1,7 @@
 using Cnblogs.Architecture.Ddd.Cqrs.Abstractions;
 using Cnblogs.Architecture.Ddd.Domain.Abstractions;
 using Cnblogs.Architecture.Ddd.Infrastructure.Abstractions;
+using Cnblogs.Architecture.IntegrationTestProject.Infrastructure;
 using Cnblogs.Architecture.IntegrationTestProject.Models;
 using Mapster;
 
@@ -9,12 +10,10 @@ namespace Cnblogs.Architecture.IntegrationTestProject.Application.Queries;
 public class ListArticlesQueryHandler<TDto> : IPageableModelQueryHandler<ListArticlesQuery<TDto>, TDto>
     where TDto : IModel
 {
-    private static readonly ArticleDto[] Articles = [new() { Id = 1, Title = "作为一个高中生开发者，我的所思所想" }];
-
     /// <inheritdoc />
     public Task<PagedList<TDto>> Handle(ListArticlesQuery<TDto> request, CancellationToken cancellationToken)
     {
-        var dto = Articles.Adapt<List<TDto>>();
-        return Task.FromResult(new PagedList<TDto>(dto, request.PagingParams, Articles.Length));
+        var dto = TestData.Articles.Adapt<List<TDto>>();
+        return Task.FromResult(new PagedList<TDto>(dto, request.PagingParams, TestData.Articles.Length));
     }
 }
